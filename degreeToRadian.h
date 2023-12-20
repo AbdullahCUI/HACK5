d477a26497a6caf610567bb5f9001beb5fb68ace
Abdullah-0110 <abdullahdev88@gmail.com>
2023-12-21 00:46:41 +0500

    Time delegation.h

--- degreeToRadian.h
+++ degreeToRadian.h
@@ -6,11 +6,3 @@ double degreesToRadians(double degree){
     return degree;
 }
 
-//Timedelegation.c
- // i made lorentzTimeDilation() function for to find time
- #include<math.h>
- double lorentzTimeDilation(double t, double percentC){
-    double T;
-    T = t/sqrt(1.0 - pow(percentC, 2));
-    return T;
- }
