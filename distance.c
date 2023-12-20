18c848a30e152a280c8f095be46bbc71053577b0
Abdullah-0110 <abdullahdev88@gmail.com>
2023-12-21 00:30:01 +0500

    spherical law

--- distance.c
+++ distance.c
@@ -7,3 +7,12 @@ double getAirDistance(double originLatitude,double originLongitude,
     // use self maded degreesToRadians Function convert degrees to radian which present in object file                   
     originLatitude = degreesToRadians(originLatitude);
     originLongitude = degreesToRadians(originLongitude);
+     destinationLatitude = degreesToRadians(destinationLatitude);
+    destinationLongitude = degreesToRadians(destinationLongitude);
+    double delta = destinationLongitude - originLongitude;
+    float const R = 6371;
+    // spherical law
+    double distance = acos(sin(originLatitude) * sin(destinationLatitude) + cos(originLatitude) * cos(destinationLatitude) *cos(delta)) * R;
+    // return distance
+    return distance;
+}
